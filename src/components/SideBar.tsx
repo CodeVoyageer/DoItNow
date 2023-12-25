0e4ecb2d083831d0928340da4953b42f3710bc3f
Roman Wróblewski <wroblewski2003r@wp.pl>
2023-12-25 13:16:58 +0100

    basic sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '../styles/SideBar.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faList,faStar,faBriefcase,faGift,faBirthdayCake,faPlus,faCheck,faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar: React.FC = () => {
   return (
@@ -13,12 +13,14 @@ const SideBar: React.FC = () => {
         <div className="sidebar_container">
           <ul className="sidebar_list">
             <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faUser}/></span>Profile</li>
-            <li className="sidebar_list_menu">Wszystko</li>
-            <li className="sidebar_list_menu">Zadania z gwiazdką</li>
-            <li className="sidebar_list_menu">Praca</li>
-            <li className="sidebar_list_menu">Lista życzeń</li>
-            <li className="sidebar_list_menu">Urodziny</li>
-            <li className="sidebar_list_menu">Dodaj zadanie</li>
+            <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faList}/></span>Wszystko</li>
+            <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faStar}/></span>Zadania z gwiazdką</li>
+            <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faBriefcase}/></span>Praca</li>
+            <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faGift}/></span>Lista życzeń</li>
+            <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faBirthdayCake}/></span>Urodziny</li>
+            <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faPlus}/></span>Dodaj zadanie</li>
+            <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faCheck}/></span>Zrobione</li>
+            <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faTrashCan}/></span>Kosz</li>
           </ul>
         </div>
       </aside>
