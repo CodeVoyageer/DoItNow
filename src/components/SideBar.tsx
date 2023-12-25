d316ca8482ddc1f12e2ec841699b7195f816bcd1
Roman Wróblewski <wroblewski2003r@wp.pl>
2023-12-25 12:34:27 +0100

    Sidebar list

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-
-const SideBar:React.FC = () => {
+import '../styles/SideBar.scss'
+const SideBar: React.FC = () => {
   return (
     <>
-      <aside>
-        <div>
-          <h1>DoItNow</h1>
+      <aside className="sidebar">
+        <div className="sidebar_logo-container">
+          <h1 className="sidebar_logo">Do<span className="sidebar_logo_second">It</span>Now</h1>
+        </div>
+        <div className="sidebar_container">
+          <ul className="sidebar_list">
+            <li className="sidebar_list_menu">Profile</li>
+            <li className="sidebar_list_menu">Wszystko</li>
+            <li className="sidebar_list_menu">Zadania z gwiazdką</li>
+            <li className="sidebar_list_menu">Praca</li>
+            <li className="sidebar_list_menu">Lista życzeń</li>
+            <li className="sidebar_list_menu">Urodziny</li>
+            <li className="sidebar_list_menu">Dodaj zadanie</li>
+          </ul>
         </div>
       </aside>
     </>
