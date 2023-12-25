5920eaaa160ef5b78128813a3b0e083e035476cb
Roman Wróblewski <wroblewski2003r@wp.pl>
2023-12-25 12:53:09 +0100

    pierwsza ikona

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import '../styles/SideBar.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 const SideBar: React.FC = () => {
   return (
     <>
       <aside className="sidebar">
         <div className="sidebar_logo-container">
-          <h1 className="sidebar_logo">Do<span className="sidebar_logo_second">It</span>Now</h1>
+          <h1 className="sidebar_logo">Do<p className="sidebar_logo_second">It</p>Now</h1>
         </div>
         <div className="sidebar_container">
           <ul className="sidebar_list">
-            <li className="sidebar_list_menu">Profile</li>
+            <li className="sidebar_list_menu"><span className="sidebar_list_icon"><FontAwesomeIcon icon={faUser}/></span>Profile</li>
             <li className="sidebar_list_menu">Wszystko</li>
             <li className="sidebar_list_menu">Zadania z gwiazdką</li>
             <li className="sidebar_list_menu">Praca</li>
