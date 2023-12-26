7f1c28fe143b8be113557040a48b37d5162657f4
Roman Wróblewski <wroblewski2003r@wp.pl>
2023-12-26 13:33:04 +0100

    surowa tabela do stylowania

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -5,23 +5,25 @@ const AddTask: React.FC = () => {
   return (
     <>
       <section className="addTask">
+        <div className="flex-row">
         <div className="addTask_first-container">
           <h1 className="addTask_heading">Dodaj swoje zadanie</h1>
           <button className="addTask_button">Dodaj zadanie</button>
         </div>
-        <div>
-          <table>
-            <tr>
-              <th>Nazwa</th>
-              <th>Kategoria</th>
-              <th>Data</th>
+        <div className="table-container">
+          <table className="table">
+            <tr className="table_heading-row">
+              <th className="table_heading">Nazwa</th>
+              <th className="table_heading">Kategoria</th>
+              <th className="table_heading">Data</th>
             </tr>
-            <tr>
-              <td>Zrobić tabele</td>
-              <td>Praca</td>
-              <td>26.12.2023</td>
+            <tr className="table_row">
+              <td className="table_last_task">Zrobić tabele</td>
+              <td className="table_last_task">Praca</td>
+              <td className="table_last_task">26.12.2023</td>
             </tr>
           </table>
+        </div>
         </div>
       </section>
     </>
